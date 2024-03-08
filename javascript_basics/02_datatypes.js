a5907f31bc582c4c571e5643458834de84c41eb0
Milan Mohan Ekanna <milanmekanna@gmail.com>
2024-03-08 12:21:47 +0000

    datatypes

---
+++ javascript_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//alert(3+3) // we are using nodejs, not browser
+
+/*
+  number -> 2 to power 53
+  bigint
+  string = ""
+  boolean => true/false;
+  null => standalone value
+  undefined =>
+  symbol = to find uniqueness (more in react)
+  // These are primitive datatype
+
+
+  //object
+
+*/
+console.log(typeof undefined); //->> undefined
+console.log(typeof null);      //->> object
+console.log(typeof Array);      //->> function
+console.log(typeof String);      //->> function
