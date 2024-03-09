a804b8e083479e7fb88c5f0dcb5fae12bb55199b
Milan Mohan Ekanna <milanmekanna@gmail.com>
2024-03-09 05:19:55 +0000

    conversions of datatypes

---
+++ javascript_basics/03_conversionsOperations.js
@@ -0,0 +1,18 @@
+let score = undefined
+let score1 = "33abc"
+
+console.log( typeof (score)) 
+console.log( typeof (score1)) 
+
+let valueInNumber = Number(score1)
+console.log( typeof (valueInNumber)) 
+console.log( valueInNumber) 
+
+//"33" => number("33") = 33
+//"33abc" => number("33") = number but this number type is NAN
+//"" => false
+//"Milan" => True
+// 1 => True // 0 => false
+
+//null = object
+//undefined = undefined
